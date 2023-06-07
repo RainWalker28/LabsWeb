6bb92cbb88daeaa6af226c91fe11d66358f96110
RainWalker28 <68481130+RainWalker28@users.noreply.github.com>
2023-06-07 21:14:58 +0200

    Delete apps directory

--- apps/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from apps.models import User, Product, Order, OrderItem, ShippingAddress
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(OrderItem)
-admin.site.register(ShippingAddress)
